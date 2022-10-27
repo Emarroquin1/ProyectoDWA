77b9a57510b15097fac912599a0f765011feb714
Emarroquin1 <82725260+Emarroquin1@users.noreply.github.com>
2022-10-27 11:26:58 -0600

    mapa update

--- Java/ubicacion.js
+++ Java/ubicacion.js
@@ -1,52 +1,32 @@
 
-/** @type {HTMLElement}*/
-var posElt;
-/** @type {HTMLElement} */
-var posLinkElt;
-
-
 window.addEventListener('load', function(){
-    posElt = document.getElementById('Pos');
-    posLinkElt = document.querySelector('#PosLink > a');
+   
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(pos){
+             var lat = pos.coords.latitude;
+             var lon = pos.coords.longitude;
+             //creacion del mapa
+             const map= L.map('map').setView([lat,lon],15);
+             //moisaico de capas
+             const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+                 attribution:'<img  style="width: 75px; height: 45px;"src="images/logo.png" alt="">',
+                 maxZoom:20
+             }).addTo(map)
 
-    // if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(geoposOK, geoposKO);
-    // }
-    // else {
-    // }
+             //creacion de marcador
+             const marker = L.marker([lat,lon],{alt:'Ubicación Buscada'}).addTo(map).bindPopup('TU ESTAS AQUI').openPopup();
+
+
+
+        });
+
+
+
+
+     }else{
+         console.log("El navegador no permite Geolocalización");
+     }
+
 });
-
-/** @param {GeolocationPosition} pos */
-function geoposOK(pos) {
-    //Obtenemos latitud y longitud
-    var lat = pos.coords.latitude;
-    var long = pos.coords.longitude;
-    //Mostramos la posición
-    posElt.textContent = `${lat}, ${long}`;
-    //generamos enlace a la posición
-    posLinkElt.href = `https://maps.google.com/?q=${lat},${long}`;
-    posLinkElt.textContent = 'Mostrar tu posición en un mapa';
-}
-
-/** @param {GeolocationPositionError} err */
-function geoposKO(err) {
-    console.warn(err.message);
-    let msg;
-    switch(err.code) {
-        case err.PERMISSION_DENIED:
-            msg = "No nos has dado permiso para obtener tu posición";
-            break;
-        case err.POSITION_UNAVAILABLE:
-            msg = "Tu posición actual no está disponible";
-            break;
-         case err.TIMEOUT:
-             msg = "No se ha podido obtener tu posición en un tiempo prudencial";
-             break;
-         default:
-             msg = "Error desconocido";
-             break;
-    }
-    posElt.textContent = msg;
-}
 
 
