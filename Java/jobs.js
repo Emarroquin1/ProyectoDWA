8589a220d47aa1e28e72fd8ab31e916028e6f7f6
Emarroquin1 <82725260+Emarroquin1@users.noreply.github.com>
2022-10-30 15:31:40 -0600

    update developers and jobs

---
+++ Java/jobs.js
@@ -0,0 +1,22 @@
+/*const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1ab3a0ceb1msh6a9c1937f6fa475p150d73jsnafb7fa0e2e9d',
+		'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+	}
+};
+
+fetch('https://jsearch.p.rapidapi.com/search?query=Python%20developer%20in%20Texas%2C%20USA&num_pages=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+    */
+
+var employer = document.getElementById('employer');
+var Jobpublisher = document.getElementById('Jobpublisher');
+var Jobdescription = document.getElementById('Jobdescription');
+var url = document.getElementById('url');
+var url = document.getElementById('url');
+
+url.innerHTML += '<a href="">Apply</a>';
