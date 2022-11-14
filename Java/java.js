300e4003406ea003b0affc0d693aedafc75324b8
Emarroquin1 <82725260+Emarroquin1@users.noreply.github.com>
2022-11-14 13:53:19 -0600

    redes sociales

--- Java/java.js
+++ Java/java.js
@@ -26,3 +26,28 @@ var h=0;
 
 
       
+
+  
+   
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "102479432693468");
+        chatbox.setAttribute("attribution", "biz_inbox");
+    
+  
+
+   
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v15.0'
+          });
+        };
+  
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    
